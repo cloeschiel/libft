e95d5552b7050d666efa5fa104c95385bff64b42
Cloe Schiel <cschiel@k2r2p6.42mulhouse.fr>
2024-01-04 17:42:55 +0100

    oui

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: cschiel <cschiel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 16:51:17 by cschiel           #+#    #+#             */
-/*   Updated: 2023/12/19 18:16:32 by cschiel          ###   ########.fr       */
+/*   Updated: 2024/01/04 17:41:34 by cschiel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,29 @@
 
 int	ft_atoi(const char *nptr)
 {
-    int	
+	int	i;
+	int	sign;
+	int	result;
+
+	if (!nptr)
+		return (0);
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (nptr[i] == ' ' || (9 >= nptr[i] && (nptr[i] <= 13)))
+		i++;
+	if (nptr[i] == '+')
+		i++;
+	else if (nptr[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	while (nptr[i] != '\0' && nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		result *= 10;
+		result += nptr[i++] - 48;
+	}
+	result *= sign;
+	return (result);
 }
