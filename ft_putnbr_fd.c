bb4a92cb686f975b81e40b283a2deae59d4cd684
Cloe Schiel <cschiel@k2r3p5.42mulhouse.fr>
2024-01-12 20:36:23 +0100

    oui

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: cschiel <cschiel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 16:47:43 by cschiel           #+#    #+#             */
-/*   Updated: 2024/01/10 21:23:50 by cschiel          ###   ########.fr       */
+/*   Updated: 2024/01/12 20:27:27 by cschiel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,36 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n < 0)
+	if (fd < 0)
+		return ;
+	if (n == -2147483648)
+		ft_putstr_fd("-2147483648", fd);
+	if (n < 0 && n != -2147483648)
 	{
-		if (n == -214783648)
-		{
-			ft_putstr_fd("-214783648", fd);
-			return ;
-		}
 		ft_putchar_fd('-', fd);
-		n = -n;
+		n *= -n;
 	}
-	if (n / 10)
+	if (n >= 10)
 	{
 		ft_putnbr_fd(n / 10, fd);
 		ft_putnbr_fd(n % 10, fd);
 	}
-	else
-		ft_putchar_fd('0' + n, fd);
+	if (n >= 0 && n < 10)
+		ft_putchar_fd(n + '0', fd);
 }
+// void	ft_putnbr_fd(int n, int fd)
+// {
+// 	long	nb;
+
+// 	nb = n;
+// 	if (n < 0)
+// 	{
+// 		ft_putchar_fd('-', fd);
+// 		nb = -nb;
+// 	}
+// 	if (nb >= 10)
+// 		ft_putnbr_fd(nb / 10, fd);
+// 	else if (nb > 10)
+// 		ft_putchar_fd((nb / 10) + '0', fd);
+// 	ft_putchar_fd((nb % 10) + '0', fd);
+// }
